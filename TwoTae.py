3bea58523536f234c2db86ce3255f5e37b6e00fd
2T-ae <66900741+2T-ae@users.noreply.github.com>
2021-05-20 13:35:58 +0900

    Update TwoTae.py

--- TwoTae.py
+++ TwoTae.py
@@ -1,33 +1,37 @@
 import discord
-import asyncio
+from discord import message
+from discord import colour
+from discord.colour import Color
+from discord.embeds import Embed
 from discord.ext import commands, tasks
+import asyncio
+import random
 import os
+
+from discord.mentions import AllowedMentions
 
 intents = discord.Intents.all()
 intents.members = True 
 bot = commands.Bot(command_prefix='&', intents = intents)
 bot.remove_command('help')
 idchannel = 837332811831967804
+tae = 298333126143377419
 
 @bot.event
 async def on_ready():
     # 프로그램 실행 시 초기 구성
-    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name="Do My Best by D.Ark"))  
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="&도움말 을 통해 명령어를 사용해보세요!"))  
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
 
-@bot.command()
-async def 안녕(ctx):
-    await ctx.send('안녕!')
-
-@bot.command()
+@bot.command()  
 async def 도움말(ctx):
     embed = discord.Embed(title='도움말', description=' ', color=0xFAFD40)
     embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/837952773395841024/837952822527393802/rankong.png')
     embed.set_footer(text='Made By 2Tae#0001', icon_url='https://cdn.discordapp.com/attachments/837952773395841024/837952822527393802/rankong.png')
-    embed.add_field(name='Command', value='`&help command`', inline=True)
+    embed.add_field(name='Commands', value='`&help command`', inline=True)
     embed.add_field(name='Music', value='`&help music`', inline=True)
     await ctx.send(embed = embed)
 
@@ -35,10 +39,11 @@ async def 도움말(ctx):
 async def help(ctx, arg):
     if arg == 'command':
         # help commands를 사용했을때 출력 될 임베드
-        embed = discord.Embed(title='Command', description='Tae Bot Commands', color=0xFAFD40)
+        embed = discord.Embed(title='Commands', description='Tae Bot Commands', color=0xFAFD40)
         embed.set_footer(text='Made By 2Tae#0001', icon_url='https://cdn.discordapp.com/attachments/837952773395841024/837952822527393802/rankong.png')
         embed.add_field(name='&도움말', value='`도움말을 확인할 수 있습니다.`', inline=True)
         embed.add_field(name='&DM', value='`이테를 디엠으로 부를 수 있습니다.`', inline=True)
+        embed.add_field(name='&모여', value='`&모여 <할 것> 을 통해 서버에 있는 유저들을 호출 할 수 있습니다.`', inline=True)
         await ctx.send(embed = embed)
     if arg == 'music':
         # help music를 사용했을때 출력 될 임베드
@@ -56,6 +61,43 @@ async def DM(ctx):
     else:
         await bot.get_user(tae).send(f'{user.mention}님, {ctx.message.author}님이 DM 명령어를 사용했습니다!')
         await ctx.send('2Tae님이 온라인 상태여서 DM을 전송했습니다.')
+
+@bot.command()
+async def 노래추천(ctx):
+    try:
+        dm_channel = await ctx.message.author.create_dm()
+        embed = discord.Embed(title='[ENG] [고등래퍼4/최종회] 김우림 - Do My Best (Feat. 제시) @ 파이널 | Mnet 210423 방송', url='https://www.youtube.com/watch?v=kc7t4s78Hok', description='', color=0xFAFD40)
+        embed.set_author(name='Mnet Official', icon_url='https://yt3.ggpht.com/ytc/AAUvwngh2Ctucs27ygguTKMB21kuat1zOoyvL41UFBtDxQ=s48-c-k-c0x00ffffff-no-rj')
+        embed.set_thumbnail(url='https://musicmeta-phinf.pstatic.net/album/005/691/5691474.jpg?type=r120Fll&v=20210427090509')
+        embed.set_image(url='https://i.ytimg.com/vi/kc7t4s78Hok/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBRIS1jpMSzFyyNbH3mkGVycsbzjw')
+        embed.set_footer(text='Made By 2Tae#0001', icon_url='https://cdn.discordapp.com/attachments/837952773395841024/837952822527393802/rankong.png')
+        await dm_channel.send(f'{ctx.message.author.mention}님, 오늘의 추천 노래입니다!')
+        await dm_channel.send(embed = embed)
+        await ctx.send(f'{ctx.message.author.mention} 성공적으로 DM을 전송했습니다!')
+    except:
+        await ctx.send(f'{ctx.message.author.mention} DM을 전송하는데 실패했습니다. 디스코드 설정에서 `개인정보 보호 및 보안`에 들어가서 `서버 멤버가 보내는 개인 메세지 허용하기`를 켜주신 후에 다시 시도해주세요!')
+        return
+
+@bot.command()
+async def 청소(ctx,amount:int):
+    try:
+        await ctx.channel.purge(limit=int(amount+1))
+        await ctx.send(f'{amount}개의 메세지를 청소했습니다!')
+    except ValueError:
+        await ctx.send('청소 할 메세지의 수를 입력해주세요!')
+        return
+
+@bot.command(name='avatar', aliases=['av'])
+async def _avatar(ctx, member : discord.Member=None):
+    if member is None:
+        embed = discord.Embed(title='이 명령어의 사용법은 `&avatar or av @member`입니다', colour=0xff0000, timestamp=ctx.message.created_at)
+        await ctx.send(embed = embed)
+        return
+    else:
+        embed2 = discord.Embed(title='Avatar', description='')
+        embed2.set_image(url=member.avatar_url)
+        embed2.set_author(name=f'{member}', icon_url=member.avatar_url)
+        await ctx.send(embed = embed2)
 
 @bot.event
 async def on_member_join(member):
