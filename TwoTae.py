e15b170f42d3a3498f76f82828c3a81d32ebbd62
2T-ae <66900741+2T-ae@users.noreply.github.com>
2021-05-20 21:27:41 +0900

    Update TwoTae.py

--- TwoTae.py
+++ TwoTae.py
@@ -1,4 +1,9 @@
 import discord
+import asyncio
+import random
+import os
+import youtube_dl
+import shutil
 from discord import message
 from discord import colour
 from discord import channel
@@ -7,10 +12,6 @@ from discord.embeds import Embed
 from discord.ext import commands, tasks
 from discord.flags import alias_flag_value
 from discord.utils import get
-import asyncio
-import random
-import os
-import youtube_dl
 from discord.ext.commands.errors import MissingPermissions
 
 from discord.mentions import AllowedMentions
@@ -168,21 +169,61 @@ async def leave(ctx):
 @bot.command(pass_context=True, aliases=['p'])
 # 노래 재생
 async def play(ctx, url: str):
-    channel = ctx.message.author.voice.channel
-    voice = get(bot.voice_clients, guild=ctx.guild)
-    song_there = os.path.isfile("song.mp3")
     
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
+    def check_queue():
+        Queue_infile = os.path.isdir("./Queue")
+        if Queue_infile is True:
+            DIR = os.path.abspath(os.path.realpath("Queue"))
+            length = len(os.listdir(DIR))
+            still_q = length - 1
+            try:
+                first_file = os.listdir(DIR)[0]
+            except:
+                print("No more queued song(s)\n")
+                queues.clear()
+                return
+            main_location = os.path.dirname(os.path.realpath(__file__))
+            song_path = os.path.abspath(os.path.realpath("Queue") + "\\" + first_file)
+            if length != 0:
+                print("Song done, playing next queued\n")
+                print(f"Songs still in queue: {still_q}")
+                song_there = os.path.isfile("song.mp3")
+                if song_there:
+                    os.remove("song.mp3")
+                shutil.move(song_path, main_location)
+                for file in os.listdir("./"):
+                    if file.endswith(".mp3"):
+                        os.rename(file, 'song.mp3')
+
+                voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
+                voice.source = discord.PCMVolumeTransformer(voice.source)
+                voice.source.volume = 0.06
+
+            else:
+                queues.clear()
+                return
+
+        else:
+            queues.clear()
     
+
+    song_there = os.path.isfile("song.mp3")    
     try:
         if song_there:
             os.remove("song.mp3")
+            queues.clear()
     except PermissionError:
         await ctx.send('ERROR: Music playing')
         return
+
+
+    Queue_infile = os.path.isdir("./Queue")
+    try:
+        Queue_folder = "./Queue"
+        if Queue_infile is True:
+            shutil.rmtree(Queue_folder)
+    except:
+        await ctx.send("모든 준비가 끝났습니다")
 
     voice = get(bot.voice_clients, guild=ctx.guild)
 
@@ -204,7 +245,7 @@ async def play(ctx, url: str):
             name = file
             os.rename(file, "song.mp3")
 
-    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print(''))
+    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
     voice.source = discord.PCMVolumeTransformer(voice.source)
     voice.source.volume = 0.06
 
@@ -241,8 +282,9 @@ async def resume(ctx):
 
 @bot.command(pass_context=True, aliases=['s', 'ski'])
 async def skip(ctx):
-
     voice = get(bot.voice_clients, guild=ctx.guild)
+
+    queues.clear()
 
     if voice and voice.is_playing():
         voice.stop()
@@ -251,6 +293,41 @@ async def skip(ctx):
     else:
         embed = discord.Embed(title='스킵할 노래가 없습니다.', description=' ', color=0xFAFD40)
         await ctx.send(embed = embed)
+
+queues = {}
+
+@bot.command(pass_context=True, aliases=['q', 'que'])
+async def queue(ctx, url: str):
+    Queue_infile = os.path.isdir("./Queue")
+    if Queue_infile is False:
+        os.mkdir("Queue")
+    DIR = os.path.abspath(os.path.realpath("Queue"))
+    q_num = len(os.listdir(DIR))
+    q_num += 1
+    add_queue = True
+    while add_queue:
+        if q_num in queues:
+            q_num += 1
+        else:
+            add_queue = False
+            queues[q_num] = q_num
+
+    queue_path = os.path.abspath(os.path.realpath("Queue") + f"\song{q_num}.%(ext)s")
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'quiet': True,
+        'outtmpl': queue_path,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading audio now\n")
+        ydl.download([url])
+        await ctx.send("노래를 " + str(q_num) + "번에 추가했습니다.")
 
 
 @bot.event
